c220724ae021d0692689c698892c20d1be6f2c6c
0xsweat <83222877+0xsweat@users.noreply.github.com>
2023-06-17 21:44:04 -0700

    bug fixes

--- sweatDB/__init__.py
+++ sweatDB/__init__.py
@@ -15,15 +15,16 @@ class actions :
     
     # Delete an item or database.
     
-    def delete(db,delete_type,item=''):
+    def delete(db,item='',delete_type='item'):
         if e(db) != True:
             return f"Database : {db} doesn't exist"
         if delete_type == "db":
             os.remove(db)
         elif item == '':
             raise Exception('No item specified')
+        else:
             a = open(db, 'r').read().split("\n")
-            open(db, 'w').write("".join([f"{x}\n" for x in a if x.startswith(f'{item} ') == False])[:-1])
+            open(db, 'w').write("".join([f"{x}\n" for x in a if x.startswith(f'{item}') == False])[:-1])
 
     # Add an item to a database.
 
@@ -44,15 +45,14 @@ class actions :
             if start < end and start > 0:
                 return ''.join(f'{x}\n' for x in c.split("\n")[start + 1:end + 2])
             else:
-                return c
+                return c[:-1]
         elif option == "item":
-            b = open(db, 'r').read().split("\n")
+            b = open(db, 'r').read().split("\n")[1:]
             output = ''
-            for i in range(1,len(b)):
-                if b[i].startswith(f'{item}'):
-                    output += ''.join([f'{x} ' for x in b[i].split(" ")[1:]])[:-1] + "\n"
+            for i in b:
+                if i.startswith(f'{item}'):
+                    output += ''.join([f'{x} ' for x in i.split(" ")[1:]])[:-1] + "\n"
             return output
-            raise Exception(f'{item} not present in {db}')
         elif option == "items":
             if start > end and start == 1:
                 return(''.join([f'{x.split(" ")[0]}\n' for x in open(db, 'r').read().split('\n')[1:]])[:-1])
@@ -62,14 +62,28 @@ class actions :
             return(open(db, 'r').read().split("\n")[0])
         elif option == "count":
             return(open(db, 'r').read().count("\n") - 1)
+        elif option == "iv":
+            b = open(db, 'r').read().split("\n")[1:]
+            output = ''
+            if item != '':
+                for x in b:
+                    if x.startswith(f'{item}'):
+                        output += f'{x}\n'
+                return output
+            else:
+                return(''.join([f'{x}\n' for x in open(db, 'r').read().split('\n')[1:]])[:-1])
+        else:
+            return(''.join([f'{x}\n' for x in open(db, 'r').read().split('\n')[1:]])[:-1])
                 
     # Edit an item in a database.
 
-    def edit(db,item,value):
+    def edit(db,item,value,t='value'):
         if e(db) != True:
             return f'Database {db} does not exist'
         b = open(db, 'r').read().split("\n")
         for x in range(len(b)):
-            if b[x].startswith(f'{item} '):
-                b[x] = f"{item} {newvalue}"
+            if b[x].startswith(f'{item}') and t=='value':
+                b[x] = f"{item} {value}"
+            elif b[x].startswith(f'{item}') and t!='value':
+                b[x] = f"{value} {''.join(f'{a} ' for a in [b[x].split(' ')[1:]])[:-1]}"
         open(db, 'w').write("".join([f"{x}\n" for x in b])[:-1])
