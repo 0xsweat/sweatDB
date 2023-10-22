89cd0d4b6dc58075b18dc64d9b148f92b43e9d12
0xsweat <83222877+0xsweat@users.noreply.github.com>
2023-10-22 02:47:48 -0700

    Switched out ugly if statements for one switch

--- sweatDB/__init__.py
+++ sweatDB/__init__.py
@@ -1,14 +1,13 @@
 import datetime
 import os
 import lzma
-
 version = "v2.0"
 e = os.path.isfile
 lf = lzma.LZMAFile
-
 class actions :
     
     # Create a database.
+    
     def create(db):
         if e(db):
             raise Exception(f"{db} already exists")
@@ -17,6 +16,7 @@ class actions :
             lf(db,'wb').write(a)
 
     # Delete an item or database.
+    
     def delete(db,item='',delete_type='item'):
         if e(db) != True:
             return f"Database : {db} doesn't exist"
@@ -31,62 +31,62 @@ class actions :
             with lf(db, 'wb')as f:
                 f.write(bytes("".join([f"{x}\n" for x in a if x.startswith(f'{item}') == False])[:-1],'utf-8'))
                 f.close()
-                
     # Add an item to a database.
+
     def add(db,name,value):
-        try:
-            if e(db):
-                with lf(db,'ab') as f:
-                    f.write(bytes(f'{name} {value}\n','utf-8'))
-                    f.close()
-            else:
-                pass
-        except:
-            print(f"Database {db} not found")
+        if e(db):
+            with lf(db,'ab') as f:
+                f.write(bytes(f'{name} {value}\n','utf-8'))
+                f.close()
+        else:
+            raise Exception(f'Database {db} not found')
 
     # View ALL items, An item, item names, info, item count, and you can specify the limit with start && end.
+
     def view(db,option='all',item='',start=1,end=0):
         option = option.lower()
         if e(db) != True:
             raise Exception(f'Database {db} not found')
-        elif option == "all":
-            c = str(lf(db, 'rb').read(),'utf-8')
-            if start < end and start > 0:
-                return ''.join(f'{x}\n' for x in c.split("\n")[start + 1:end + 2])
-            else:
-                return c[:-1]
-        elif option == "item":
-            with lf(db, 'r')as f:
-                b = str(f.read(),'utf-8').split("\n")[1:]
-                f.close()
-            output = ''
-            for i in b:
-                if i.startswith(f'{item}'):
-                    output += ''.join([f'{x} ' for x in i.split(" ")[1:]])[:-1] + "\n"
-            return output
-        elif option == "items":
-            if start > end and start == 1:
-                return(''.join([f'{x.split(" ")[0]}\n' for x in str(lf(db, 'rb').read(),'utf-8').split('\n')[1:]])[:-1])
-            else:
-                return(''.join([f'{x.split(" ")[0]}\n' for x in str(lf(db, 'rb').read(),'utf-8').split('\n')[start + 1:end + 2]])[:-1])
-        elif option == "info":
-            return(str(lf(db, 'rb').read(),'utf-8').split("\n")[0])
-        elif option == "count":
-            return(str(lf(db, 'rb').read(),'utf-8').count("\n") - 1)
-        elif option == "iv":
-            b = str(lf(db, 'rb').read(),'utf-8').split("\n")[1:]
-            output = ''
-            if item != '':
-                for x in b:
-                    if x.startswith(f'{item}'):
-                        output += f'{x}\n'
+        match option:
+            case "all":
+                c = str(lf(db, 'rb').read(),'utf-8')
+                if start < end and start > 0:
+                    return ''.join(f'{x}\n' for x in c.split("\n")[start + 1:end + 2])
+                else:
+                    return c[:-1]
+            case "item":
+                with lf(db, 'r')as f:
+                    b = str(f.read(),'utf-8').split("\n")[1:]
+                    f.close()
+                output = ''
+                for i in b:
+                    if i.startswith(f'{item}'):
+                        output += ''.join([f'{x} ' for x in i.split(" ")[1:]])[:-1] + "\n"
                 return output
-            else:
+            case "items":
+                if start > end or start == 1 or type(start) != type(1) or type(end) != type(1):
+                    return(''.join([f'{x.split(" ")[0]}\n' for x in str(lf(db, 'rb').read(),'utf-8').split('\n')[1:]])[:-1])
+                else:
+                    return(''.join([f'{x.split(" ")[0]}\n' for x in str(lf(db, 'rb').read(),'utf-8').split('\n')[start + 1:end + 2]])[:-1])
+            case "info":
+                return(str(lf(db, 'rb').read(),'utf-8').split("\n")[0])
+            case "count":
+                return(str(lf(db, 'rb').read(),'utf-8').count("\n") - 1)
+            case "iv":
+                b = str(lf(db, 'rb').read(),'utf-8').split("\n")[1:]
+                output = ''
+                if item != '':
+                    for x in b:
+                        if x.startswith(f'{item}'):
+                            output += f'{x}\n'
+                    return output
+                else:
+                    return(''.join([f'{x}\n' for x in str(lf(db, 'rb').read(),'utf-8').split('\n')[1:]])[:-1])
+            case _:
                 return(''.join([f'{x}\n' for x in str(lf(db, 'rb').read(),'utf-8').split('\n')[1:]])[:-1])
-        else:
-            return(''.join([f'{x}\n' for x in str(lf(db, 'rb').read(),'utf-8').split('\n')[1:]])[:-1])
                 
     # Edit an item in a database.
+
     def edit(db,item,value,t='value'):
         if e(db) != True:
             return f'Database {db} does not exist'
